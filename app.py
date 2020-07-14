a8f3d5deab32cacee753cc06a89987bf864454c9
Vladimir <vadimirkosarev@gmail.com>
2020-07-14 11:35:32 +0300

    fix

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ class select_model():
         self.y_train  = y_train
         self.x_test   = x_test
         self.y_test   = y_test
-        self.my_dates = pd.date_range(start='2017-09-30',periods=len(self.y_test),freq='M')
         # assert(len(x_train)==len(y_train)and len(x_test)==len(y_test))
 
     def plot_his(self,hist):
@@ -67,15 +66,6 @@ class select_model():
         ax1.legend(loc='best',fontsize=16)
         ax1.grid()
 
-    def plot_forec_val(self,predict):
-        fig2,ax2 = plt.subplots(figsize=(18, 5))
-        # scl = StandardScaler()
-        # df_init_scal = scl.fit_transform(load_data().values)#сохраняем мат
-        # pred_inv=scl.inverse_transform(self.y_test)
-        ax2.plot(self.my_dates,self.y_test, color='black', label = 'Факт')
-        ax2.plot(self.my_dates,predict,'-.', color='blue', label = 'Прогноз')
-        ax2.legend(loc='best',fontsize=16)
-        ax2.grid()
 
     def simple_lstm(self):#,ytrain,xtest,ytest):
         ipp_1 = Input(shape=(self.n_steps, self.x_train.shape[2]),name='fact_ipp_1')
@@ -89,25 +79,11 @@ class select_model():
         history=model.fit({'fact_ipp_1': self.x_train},{'out_1':self.y_train},validation_data=({'fact_ipp_1': self.x_test},
               {'out_1':self.y_test}),epochs=200, batch_size=len(self.x_test), verbose=0)
         ###################
-
         plottr=self.plot_his(hist=history)
         st.subheader("Процесс обучения")
         st.pyplot(fig=plottr, clear_figure=True, use_container_width=True)
-
         my_predicts=model.predict(self.x_test).flatten()
-        plotfr=self.plot_forec_val(predict=my_predicts)
-        st.subheader("Прогноз на тесте")
-        st.pyplot(fig=plotfr, clear_figure=True, use_container_width=True)
-
-        my_mse=round(metrics.mean_squared_error(self.y_test, my_predicts),3)
-        st.write('Ошибка прогноза на тестовой выборке:', str(my_mse))
-
-        df = pd.DataFrame({'Фактические данные':self.y_test,
-            'Прогнозные данные':my_predicts})
-        df.index=self.my_dates
-        st.dataframe(df.T)
-
-
+        return my_predicts
 
     def simple_gru(self):
         print('x')
@@ -115,6 +91,15 @@ class select_model():
     def simple_cnn(self):
         print('x')
 
+def plot_forec_val(ytest,predict,my_dates):
+    fig2,ax2 = plt.subplots(figsize=(18, 5))
+    # scl = StandardScaler()
+    # df_init_scal = scl.fit_transform(load_data().values)#сохраняем мат
+    # pred_inv=scl.inverse_transform(self.y_test)
+    ax2.plot(my_dates,ytest, color='black', label = 'Факт')
+    ax2.plot(my_dates,predict,'-.', color='blue', label = 'Прогноз')
+    ax2.legend(loc='best',fontsize=16)
+    ax2.grid()
 
 def main():
 
@@ -137,18 +122,30 @@ def main():
 
     x_train,x_test,y_train,y_test=prep_data_2(prep_data(tickdic[company_name]))
     my_select_model=select_model(x_train,y_train,x_test,y_test,6)
+    my_dates = pd.date_range(start='2017-09-30',periods=len(y_test),freq='M')
     if lstm:
         with st.spinner('Идет обучение нейронной сети...'):
-            my_select_model.simple_lstm()
+            my_predicts=my_select_model.simple_lstm()
     if gru:
         with st.spinner('Идет обучение нейронной сети...'):
-            my_select_model.simple_gru()
+            my_predicts=my_select_model.simple_gru()
     if cnn:
         with st.spinner('Идет обучение нейронной сети...'):
-            my_select_model.simple_cnn()
+            my_predicts=my_select_model.simple_cnn()
+
     if arima:
         with st.spinner('Построение ARIMA прогноза...'):
             pass
+
+    plotfr=plot_forec_val(y_test,my_predicts,my_dates)
+    st.subheader("Прогноз на тесте")
+    st.pyplot(fig=plotfr, clear_figure=True, use_container_width=True)
+    my_mse=round(metrics.mean_squared_error(y_test, my_predicts),3)
+    st.write('Ошибка прогноза на тестовой выборке:', str(my_mse))
+    df = pd.DataFrame({'Фактические данные':y_test,
+        'Прогнозные данные':my_predicts})
+    df.index=my_dates
+    st.dataframe(df.T)
     st.success('Done!')
 
 
